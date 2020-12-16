52c821f3162975c63a26b03bc0424302cf0dab55
Josh Iverson <josh.iverson@limelighthealth.com>
2020-12-15 20:56:17 -0700

    feat: update

--- packages/navbar/webpack.config.js
+++ packages/navbar/webpack.config.js
@@ -3,30 +3,16 @@ const singleSpaDefaults = require("webpack-config-single-spa-react");
 
 module.exports = (webpackConfigEnv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: "polyglot-mf",
+    orgName: "ap-single-spa",
     projectName: "navbar",
     webpackConfigEnv,
   });
 
   return webpackMerge.smart(defaultConfig, {
-    module: {
-      rules: [
-        {
-          test: /\.css/,
-          exclude: /node_modules/,
-          use: [
-            "style-loader",
-            {
-              loader: "css-loader",
-              options: {
-                modules: {
-                  localIdentName: "navbar__[local]--[hash:base64:5]",
-                },
-              },
-            },
-          ],
-        },
-      ],
+    // customizations can go here
+    devServer: {
+      https: true,
+      port: 9001
     },
   });
 };
