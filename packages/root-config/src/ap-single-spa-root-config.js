1189638fb71a811dc843decc2c0069c1aaa4e7e1
jiverson <jiverson222@gmail.com>
2020-12-16 22:33:33 -0700

    feat: update

--- packages/root-config/src/ap-single-spa-root-config.js
+++ packages/root-config/src/ap-single-spa-root-config.js
@@ -15,6 +15,27 @@ registerApplication({
   activeWhen: ["/"],
 });
 
-start({
-  urlRerouteOnly: true,
+registerApplication({
+  name: "@ap-single-spa/account-settings",
+  app: () => loadWithoutAmd("@ap-single-spa/account-settings"),
+  activeWhen: "/settings",
 });
+
+start();
+
+// start({
+//   urlRerouteOnly: true,
+// });
+
+// A lot of angularjs libs are compiled to UMD, and if you don't process them with webpack
+// the UMD calls to window.define() can be problematic.
+function loadWithoutAmd(name) {
+  return Promise.resolve().then(() => {
+    let globalDefine = window.define;
+    delete window.define;
+    return System.import(name).then((module) => {
+      window.define = globalDefine;
+      return module;
+    });
+  });
+}
